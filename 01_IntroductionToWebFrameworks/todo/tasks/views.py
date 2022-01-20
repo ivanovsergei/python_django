8fc43559bca371d96c57afe45e8748bd4dd977af
sergei_ivanov <x463mx@gmail.com>
2022-01-20 20:12:28 +0300

    Сделано

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
-
 from django.views import View
+import random
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+        lst = ['<li>1</li>', '<li>2</li>', '<li>3</li>', '<li>4</li>', '<li>5</li>', '<li>6</li>',
+               '<li>7</li>', '<li>8</li>', '<li>9</li>', '<li>10</li>']
+
+        random_res = (random.choices(lst, k=5))
+        for i in random_res:
+            print(i)
+        print(random_res)
+        return HttpResponse(random_res)
